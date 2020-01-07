f51cf4e497d172e8c54faa1aa60b3fda4bd24470
Chris Folsom <cfols92@gmail.com>
2020-01-07 14:10:35 -0500

    changed contact system from nodemailer to sendgrid

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require("express"),
     app = express(),
-    nodemailer = require("nodemailer"),
     bodyParser = require("body-parser");
-
-require('dotenv').config();
     
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
@@ -30,39 +27,53 @@ app.get("*",function(req,res){
 });
 
 app.post("/contact",function(req,res){
-    let mailOpts,smtpTrans;
 
-    // create a transporter object named smtpTrans
-    // this transporter object can send mail
-    smtpTrans = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.GMAIL_USER,
-          pass: process.env.GMAIL_PASS
-        }
-    });
-
-    mailOpts = {
-        from: req.body.email,
-        to: process.env.GMAIL_USER,
-        subject: 'New message from contact form at chrisfolsom.herokuapp.com',
-        text: `${req.body.name} (${req.body.email}) from ${req.body.company} says: ${req.body.content}`
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+    to: 'cfols92@gmail.com',
+    from: req.body.email,
+    subject: 'New message from contact form at chrisfolsom.herokuapp.com',
+    text: `${req.body.name} (${req.body.email}) from ${req.body.company} says:\n${req.body.content}`,
+    //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
+    sgMail.send(msg);
 
-    // use the transporter object to send mail
-    smtpTrans.sendMail(mailOpts,function(err,response){
-        if(err){
-            console.log(err);
-            //res.render("contact-failure");
-            //res.send("Contact failure");
-        } else {
-            // alert("Thank you for your message! I will get back to you as soon as possible.");
-            res.redirect("/home");
-            //res.render("contact-success");
-        }
-    });
+    res.redirect("/home");
+
+    // let mailOpts,smtpTrans;
+
+    // // create a transporter object named smtpTrans
+    // // this transporter object can send mail
+    // smtpTrans = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //       user: process.env.GMAIL_USER,
+    //       pass: process.env.GMAIL_PASS
+    //     }
+    // });
+
+    // mailOpts = {
+    //     from: req.body.email,
+    //     to: process.env.GMAIL_USER,
+    //     subject: 'New message from contact form at chrisfolsom.herokuapp.com',
+    //     text: `${req.body.name} (${req.body.email}) from ${req.body.company} says: ${req.body.content}`
+    // };
+
+    // // use the transporter object to send mail
+    // smtpTrans.sendMail(mailOpts,function(err,response){
+    //     if(err){
+    //         console.log(err);
+    //         //res.render("contact-failure");
+    //         //res.send("Contact failure");
+    //     } else {
+    //         // alert("Thank you for your message! I will get back to you as soon as possible.");
+    //         res.redirect("/home");
+    //         //res.render("contact-success");
+    //     }
+    // });
 });
 
 app.listen(process.env.PORT,process.env.IP);
