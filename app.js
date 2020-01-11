c4d85f8e5c3766bda3102356e0ef777b37939a63
Chris Folsom <cfols92@gmail.com>
2020-01-11 11:00:29 -0500

    updated dependencies

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var express = require("express"),
+const express = require("express"),
     app = express(),
     bodyParser = require("body-parser");
+
+// make all ".env" variables avaiable in "process.env"
+// require('dotenv').config();
     
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
@@ -23,10 +26,10 @@ app.get("/contact",function(req,res){
 });
 
 app.get("*",function(req,res){
-    res.send("Sorry, this page does not exist. Please return to home!")
+    res.status(404).send("Sorry, this page does not exist. Please return to home!")
 });
 
-app.post("/contact",function(req,res){
+app.post("/contact",(req,res) => {
 
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -40,44 +43,10 @@ app.post("/contact",function(req,res){
     sgMail.send(msg);
 
     res.redirect("/home");
-
-    // let mailOpts,smtpTrans;
-
-    // // create a transporter object named smtpTrans
-    // // this transporter object can send mail
-    // smtpTrans = nodemailer.createTransport({
-    //     host: 'smtp.gmail.com',
-    //     port: 465,
-    //     secure: true,
-    //     auth: {
-    //       user: process.env.GMAIL_USER,
-    //       pass: process.env.GMAIL_PASS
-    //     }
-    // });
-
-    // mailOpts = {
-    //     from: req.body.email,
-    //     to: process.env.GMAIL_USER,
-    //     subject: 'New message from contact form at chrisfolsom.herokuapp.com',
-    //     text: `${req.body.name} (${req.body.email}) from ${req.body.company} says: ${req.body.content}`
-    // };
-
-    // // use the transporter object to send mail
-    // smtpTrans.sendMail(mailOpts,function(err,response){
-    //     if(err){
-    //         console.log(err);
-    //         //res.render("contact-failure");
-    //         //res.send("Contact failure");
-    //     } else {
-    //         // alert("Thank you for your message! I will get back to you as soon as possible.");
-    //         res.redirect("/home");
-    //         //res.render("contact-success");
-    //     }
-    // });
 });
 
 app.listen(process.env.PORT,process.env.IP);
 
-// app.listen(3000,function(){
+// app.listen(3000,() => {
 //     console.log("Now serving");
 // });
